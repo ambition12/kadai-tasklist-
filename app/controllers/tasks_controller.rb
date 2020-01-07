be72939c9a674a75810607bb92f3e2bd996aa13a
EC2 Default User <ec2-user@ip-172-31-91-177.ec2.internal>
2020-01-07 01:38:50 +0000

    show & Eidt Delete Button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -10,6 +10,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -46,7 +47,8 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
   
   private
